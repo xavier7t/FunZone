b29f56c0122934f0b790993281cfeb91cd63bea5
Xavier <yuxuanli.work@icloud.com>
2022-06-02 14:14:55 -0400

    Initial Commit

---
+++ FunZone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FunZone
+//
+//  Created by Xavier on 6/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
